22c19b818cfdfd7db7f9b1583cd09dd95a894ff6
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-12-31 23:19:03 +0600

    pagination added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,39 @@ import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
 import {getMoviesByTerm} from "./api/TMDB.js";
 import MovieList from "./components/MovieList";
+import Pagination from "./components/Pagination";
 
 const App =() => {
   const [SearchTerm, setSearchTerm]=useState("");
   const [movies, setMovies]=useState([]);
+  const [currentPage, setCurrentPage]=useState(1);
+  const [totalPages, setTotalPages]=useState(0);
 
   const handleSubmit= async(event)=>{
     event.preventDefault();
-    await getMoviesByTerm(SearchTerm,setMovies);
+    await getMoviesByTerm(SearchTerm,setMovies, currentPage,setTotalPages);
   };
   const handleChange=(event)=>{
     console.log(event);
     setSearchTerm(event.target.value);
   };
+  const nextPage= async(page_number)=>{
+    setCurrentPage(page_number);
+    await getMoviesByTerm(SearchTerm,setMovies,currentPage,setTotalPages);
+  }
+
   return <div>
     <Navbar/>
     <Searchbar handleChange={handleChange} handleSubmit={handleSubmit}/>
     <MovieList movies={movies}/>
+    {totalPages>1 ? (
+    <Pagination 
+    nextPage={nextPage} 
+    currentPage={ currentPage} 
+    totalPages={totalPages}/>
+    ):
+    ("")
+    }
     </div>;
 };
 export default App;
--- src/api/TMDB.js
+++ src/api/TMDB.js
@@ -4,16 +4,18 @@ const TMDB= axios.create({
      baseURL:"https://api.themoviedb.org/3/",
 });
 
-const getMoviesByTerm=(SearchTerm,setMovies)=>{
+const getMoviesByTerm=(SearchTerm,setMovies, page_number, setTotalPages)=>{
     //const URLStr="/search/movie?api_key=7f8045ecf5aa05993377afc5969b999c&query="+SearchTerm;
     TMDB.get('search/movie',{ //URLStr
         params:{
             api_key:"7f8045ecf5aa05993377afc5969b999c",
             query: SearchTerm,
+            page: page_number
         },
     }).then((response)=>{
         console.log(response);
         setMovies(response.data.results);
+        setTotalPages(response.data.total_pages);
     });
 };
 
